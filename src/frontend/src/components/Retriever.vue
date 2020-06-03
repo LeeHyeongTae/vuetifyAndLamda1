8ba5acbabc09c3505a8230d7b3673ed2f50fbf7b
LeeHyengTae <tedd911@icloud.com>
2020-06-03 11:31:17 +0900

    11:31

--- src/frontend/src/components/Retriever.vue
+++ src/frontend/src/components/Retriever.vue
@@ -42,10 +42,10 @@
             </template>
         </v-simple-table>
         <div class="text-center">
-            <div style="margin: 0 auto; width: 500px; height: 100px">
-            <span v-if="pager.existPrev" @click="prevBlock" stlye="width: 50px; height: 50px; border: 1px solid black; margin-right: 5px">이전</span>
-            <span v-for="i of pages" :key="i" stlye="width: 50px; height: 50px; border: 1px solid black; margin-right: 5px">{{ i }}</span>
-            <span v-if="pager.existNext" @click="nextBlock" stlye="width: 50px; height: 50px; border: 1px solid black; margin-right: 5px">다음</span>
+            <div style="margin: 0 auto; width: 500px; height: 100px;">
+            <span v-if="pager.existPrev" @click="prevBlock" style="width: 50px; height: 50px; border: 1px solid black; margin-right: 5px; cursor: pointer;">이전</span>
+            <span @click="testClick(i)" v-for="i of pages" :key="i" style="width: 50px; height: 50px; border: 1px solid black; margin-right: 5px; cursor: pointer;">{{ i }}</span>
+            <span v-if="pager.existNext" @click="nextBlock" style="width: 50px; height: 50px; border: 1px solid black; margin-right: 5px; cursor: pointer;">다음</span>
 <!--    <v-pagination v-model="pageNumber" :length="5" :total-visible="5"></v-pagination>-->
             </div>
         </div>
@@ -68,11 +68,11 @@
         created() {
             axios
                 .get(`${this.$store.state.search.context}/movies/${this.$store.state.search.searchWord}/${this.$store.state.search.pageNumber}`)
-                .then(res=>{
+                .then(res => {
                     res.data.list.forEach(elem => {this.list.push(elem)})
                     this.pager = res.data.pager
                     let i = this.pager.pageStart + 1
-                    let arr = []
+                    const arr = []
                     console.log(`페이지 끝: ${this.pager.pageEnd}`)
                     for(; i<= this.pager.pageEnd + 1; i++){
                         arr.push(i)
@@ -91,10 +91,13 @@
         },
         methods: {
             prevBlock(){
-                this.pageNumber= this.pageNumber - 1
+                //
             },
             nextBlock(){
-                this.pageNumber= this.pageNumber + 1
+                //
+            },
+            testClick(d){
+                this.pageNumber = d;
             }
         }
     };
